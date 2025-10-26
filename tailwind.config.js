6b90fea1397b11fe519c3c9065623d2ef150d3ba
Daniel <daniellong1109@gmail.com>
2025-10-26 17:21:05 +0800

    Initial website upload

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class', // enable class-based dark mode so ThemeContext can toggle it
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: { extend: {} },
+  plugins: [],
+}
+
